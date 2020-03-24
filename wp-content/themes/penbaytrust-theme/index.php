eebc8552c1f0a7c0762a055a766c8422aa0485d9
spaceSmopy <frazer656@gmail.com>
2020-03-24 16:35:27 +0200

    home page main button link

--- wp-content/themes/penbaytrust-theme/index.php
+++ wp-content/themes/penbaytrust-theme/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
     <div class="bg home-page"></div>
     <section class="main-content home-page wrapper">
-        <h1 class="title">PENINSULA BAY TRUST</h1><a class="button" href="#"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
+        <h1 class="title">PENINSULA BAY TRUST</h1><a class="button" href="<?php echo site_url().'/contact/'?>"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
     </section>
