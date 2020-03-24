8baf99f1d957303542938620677c816542a3aee9
spaceSmopy <frazer656@gmail.com>
2020-03-24 09:21:50 +0200

    HP menu fix

--- wp-content/themes/penbaytrust-theme/header.php
+++ wp-content/themes/penbaytrust-theme/header.php
@@ -12,17 +12,39 @@
 
 </head>
 
-<body class="wrapper home-page">
+<body class=" home-page">
 
 <?php
 wp_body_open();
 ?>
 
-<section class="header"><a class="logo" href="<?php echo site_url();?>"><img src="<?php echo get_template_directory_uri()?>/img/svg/logo.svg" alt="logo"></a>
-        <?php wp_nav_menu( [ 'menu' => 'header_menu', 'container_class' => 'nav', ] );?>
-<!--        <ul class="nav-list">-->
-<!--            <li class="nav-list-item"><a class="nav-link" href="#">Who We Are</a></li>-->
-<!--            <li class="nav-list-item"><a class="nav-link" href="#">Services</a></li>-->
-<!--            <li class="nav-list-item"><a class="nav-link" href="#">Contact</a></li>-->
+
+
+<section class="header wrapper">
+    <a class="logo" href="#">
+        <img class="logo-big" src="<?php echo get_template_directory_uri()?>/img/svg/logo-big.svg" alt="logo-big">
+        <img class="logo-small" src="<?php echo get_template_directory_uri()?>/img/svg/logo-small.svg" alt="logo-big">
+    </a>
+    <?php wp_nav_menu( [ 'menu' => 'header_menu', 'container_class' => 'desctop-menu', ] );?>
+<!--    <nav class="">-->
+<!--        <ul class="desctop-menu__list">-->
+<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Who We Are</a></li>-->
+<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Services</a></li>-->
+<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Contact</a></li>-->
 <!--        </ul>-->
+<!--    </nav>-->
+    <div class="burger-menu">
+        <a class="burger-menu__button" href="#">
+            <span class="burger-menu__lines"></span>
+        </a>
+        <?php wp_nav_menu( [ 'menu' => 'header_menu', 'container_class' => 'burger-menu__nav', ] );?>
+<!--        <nav class="burger-menu__nav">-->
+<!--            <ul class="burger-menu__list">-->
+<!--                <li><a class="burger-menu__link" href="#">Who We Are</a></li>-->
+<!--                <li><a class="burger-menu__link" href="#">Services</a></li>-->
+<!--                <li><a class="burger-menu__link" href="#">Contact</a></li>-->
+<!--            </ul>-->
+<!--        </nav>-->
+        <div class="burger-menu__overlay"></div>
+    </div>
 </section>
--- wp-content/themes/penbaytrust-theme/index.php
+++ wp-content/themes/penbaytrust-theme/index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-    <section class="home-main-content">
+    <div class="bg home-page"></div>
+    <section class="main-content home-page wrapper">
         <h1 class="title">PENINSULA BAY TRUST</h1><a class="button" href="#"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
     </section>
 <?php get_footer(); ?>
