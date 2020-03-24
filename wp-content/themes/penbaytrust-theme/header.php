03810350d09b349f1ec9cffb55f17985d4885de1
spaceSmopy <frazer656@gmail.com>
2020-03-24 14:43:46 +0200

    menu fix

--- wp-content/themes/penbaytrust-theme/header.php
+++ wp-content/themes/penbaytrust-theme/header.php
@@ -26,25 +26,11 @@ wp_body_open();
         <img class="logo-small" src="<?php echo get_template_directory_uri()?>/img/svg/logo-small.svg" alt="logo-big">
     </a>
     <?php wp_nav_menu( [ 'menu' => 'header_menu', 'container_class' => 'desctop-menu', ] );?>
-<!--    <nav class="">-->
-<!--        <ul class="desctop-menu__list">-->
-<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Who We Are</a></li>-->
-<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Services</a></li>-->
-<!--            <li class="desctop-menu__item"><a class="desctop-menu__link" href="#">Contact</a></li>-->
-<!--        </ul>-->
-<!--    </nav>-->
     <div class="burger-menu">
         <a class="burger-menu__button" href="#">
             <span class="burger-menu__lines"></span>
         </a>
         <?php wp_nav_menu( [ 'menu' => 'header_menu', 'container_class' => 'burger-menu__nav', ] );?>
-<!--        <nav class="burger-menu__nav">-->
-<!--            <ul class="burger-menu__list">-->
-<!--                <li><a class="burger-menu__link" href="#">Who We Are</a></li>-->
-<!--                <li><a class="burger-menu__link" href="#">Services</a></li>-->
-<!--                <li><a class="burger-menu__link" href="#">Contact</a></li>-->
-<!--            </ul>-->
-<!--        </nav>-->
         <div class="burger-menu__overlay"></div>
     </div>
 </section>
