2ecb472a4e22f219cdbb785f1ca4826d377c39f6
spaceSmopy <frazer656@gmail.com>
2020-03-24 16:57:20 +0200

    contact BE fix

--- wp-content/themes/penbaytrust-theme/contact_template.php
+++ wp-content/themes/penbaytrust-theme/contact_template.php
@@ -1,13 +1,16 @@
 <?php /* Template Name: Contact Page */ ?>
-<?php get_header(); ?>
+<?php get_header();
+$post = get_post();
+$content = apply_filters('the_content', $post->post_content);
+?>
 <div class="bg contacts-page"></div>
 <section class="main-content contacts-page wrapper">
-    <h1 class="title">Contact Us</h1>
+    <h1 class="title"><?php echo get_the_title($post);?></h1>
 </section>
 <section class="contact-us wrapper">
     <div class="contact-us__info">
-        <h2 class="contact-us__title">Contact Us</h2>
-        <p class="contact-us__text">We would love to hear from you! Feel free to complete this form, or e-mail us at info@penbaytrust.com if you would like to learn more about what we do and how we might be able to help.</p>
+        <h2 class="contact-us__title"><?php echo get_the_title($post);?></h2>
+        <p class="contact-us__text"><?php echo wp_filter_nohtml_kses($content);?></p>
     </div>
     <form class="contact-us__form">
         <?php echo do_shortcode('[contact-form-7 id="20" title="Contact form"]');?>
