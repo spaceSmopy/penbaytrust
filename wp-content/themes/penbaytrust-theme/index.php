c615c114512e6a1d9e3264240f08a6cd761ad711
spaceSmopy <frazer656@gmail.com>
2020-03-24 16:39:53 +0200

    home page fix

--- wp-content/themes/penbaytrust-theme/index.php
+++ wp-content/themes/penbaytrust-theme/index.php
@@ -1,5 +1,9 @@
-<?php get_header(); ?>
+<?php get_header();
+$post = get_post();
+$content = apply_filters('the_content', $post->post_content);
+
+?>
     <div class="bg home-page"></div>
     <section class="main-content home-page wrapper">
-        <h1 class="title">PENINSULA BAY TRUST</h1><a class="button" href="<?php echo site_url().'/who-we-are/'?>"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
+        <h1 class="title"><?php echo $content?></h1><a class="button" href="<?php echo site_url().'/who-we-are/'?>"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
     </section>
