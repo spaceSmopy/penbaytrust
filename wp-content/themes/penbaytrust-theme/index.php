a24337aac9e56925dcdce78f6e399aad7895a157
spaceSmopy <frazer656@gmail.com>
2020-03-25 15:17:36 +0200

    fixes

--- wp-content/themes/penbaytrust-theme/index.php
+++ wp-content/themes/penbaytrust-theme/index.php
@@ -5,5 +5,6 @@ $content = apply_filters('the_content', $post->post_content);
 ?>
     <div class="bg home-page"></div>
     <section class="main-content home-page wrapper">
-        <h1 class="title"><?php echo $content?></h1><a class="button" href="<?php echo site_url().'/who-we-are/'?>"><img src="<?php echo get_template_directory_uri()?>/img/svg/button.svg" alt="button"></a>
+        <h1 class="title"><?php echo $content?></h1><a class="button" href="<?php echo site_url().'/who-we-are/'?>"><span class="button__lines"></span></a>
     </section>
+
